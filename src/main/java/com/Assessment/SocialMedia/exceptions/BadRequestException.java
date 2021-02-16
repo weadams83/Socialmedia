694da0916a0ad5eff6dfe4bfeb375abd9905bac5
Nathan Breedlove <nathanielbreedlove@gmail.com>
2021-02-16 16:04:02 -0500

    Create Exceptions package for custom exceptions.

---
+++ src/main/java/com/Assessment/SocialMedia/exceptions/BadRequestException.java
@@ -0,0 +1,15 @@
+package com.Assessment.SocialMedia.exceptions;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class BadRequestException extends RuntimeException{/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4623086488416716824L;
+
+	private String message;
+}
