05b887c8db89fa13c03e5a91e66199ec624f0cd1
weadams83 <willie.adamsiii@gmail.com>
2021-02-16 11:57:05 -0600

    added usercredentials

---
+++ src/main/java/com/Assessment/SocialMedia/entities/UserCredentials.java
@@ -0,0 +1,21 @@
+package com.Assessment.SocialMedia.entities;
+
+import javax.persistence.*;
+
+import lombok.Data;
+
+
+@Embeddable
+@Data
+
+public class UserCredentials {
+	
+	
+	@Column(nullable = false, unique = true)
+	private String userName;
+	
+	@Column(nullable = false)
+	private String password;
+	
+
+}
